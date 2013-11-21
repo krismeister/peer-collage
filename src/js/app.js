375f74fbc04c8709b1ce5059ce845f20f59d0b67
kmeister <kris@aliencom.net>
2013-11-20 20:05:03 -0500

    app

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,17 @@
-(function(){
-    console.log('hi');
-})();
+$(function(){
+    var peer,
+        peerId;
+    function init(){
+        peer = new Peer('master',{key: 'd1ex4hq2p3bit3xr'});
+        startConnection();
+    }
+    function startConnection(){
+        peer.on('open', function(id) {
+            console.log('My peer ID is: ' + id);
+            peerId = id;
+            $('h1').text(peerId);
+        });
+    }
+
+    init();
+});
